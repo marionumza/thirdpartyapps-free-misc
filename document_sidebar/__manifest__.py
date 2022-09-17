15806c1fb937b7b3664b8791bbafb46757bafb37
Mario Nuñez <marionumza@gmail.com>
2022-09-17 18:56:18 -0300

    add

--- document_sidebar/__manifest__.py
+++ document_sidebar/__manifest__.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 {
     'license': 'LGPL-3',
-    'name': "Document Management System",
-    'summary': "Enterprise online document management",
+    'name': "Document Sidebar",
+    'summary': "Document sidebar in form view",
+    'description': """
+Attachments list
+========================================
+* Show attachment on the top of the forms
+""",
     'author': "renjie <i@renjie.me>",
     'website': "https://renjie.me",
     'support': 'i@renjie.me',
-    'category': 'Document Management',
-    'version': '1.0',
-    'depends': ['document_sidebar', 'mail'],
+    'category': 'Extra Tools',
+    'version': '2.1',
+    'depends': ['web'],
     'data': [
-        'security/account_security.xml',
-        'security/ir.model.access.csv',
         'views/webclient_templates.xml',
-        'views/document.xml',
     ],
     'images': [
         'static/description/main_screenshot.png',
